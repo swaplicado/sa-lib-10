d5baf5dc3b884c0f30a34c53850185a683d04b3d
Sergio Flores <sflores@swaplicado.com.mx>
2023-07-13 11:45:53 -0500

    Mejoramiento de funcionalidad de cambio de cursor en SGuiUtils.

--- src/sa/lib/gui/SGuiUtils.java
+++ src/sa/lib/gui/SGuiUtils.java
@@ -27,11 +27,11 @@ import sa.lib.SLibUtils;
 public abstract class SGuiUtils {
 
     public static void setCursorWait(final SGuiClient client) {
-        client.getFrame().setCursor(new Cursor(Cursor.WAIT_CURSOR));
+        client.getFrame().getRootPane().setCursor(new Cursor(Cursor.WAIT_CURSOR));
     }
 
     public static void setCursorDefault(final SGuiClient client) {
-        client.getFrame().setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        client.getFrame().getRootPane().setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
 
     public static void setWindowBounds(final Window window, final int width, final int height) {
