4bafee131cfe2c95a8fdc91d3327bc578e62a7b5
Sergio Flores <sflores@swaplicado.com.mx>
2026-01-23 17:23:07 -0600

    Creación de constantes para identificar enero (January) y diciembre (December).

--- src/sa/lib/SLibTimeConsts.java
+++ src/sa/lib/SLibTimeConsts.java
@@ -14,6 +14,8 @@ public class SLibTimeConsts {
     public static final int YEAR_MAX = 2100;
     public static final int MONTH_MIN = 1;
     public static final int MONTH_MAX = 12;
+    public static final int MONTH_JAN = 1;
+    public static final int MONTH_DEC = 12;
     public static final int MONTHS = 12;
     public static final int WEEK_DAYS = 7;
     public static final int YEAR_DAYS = 365;
